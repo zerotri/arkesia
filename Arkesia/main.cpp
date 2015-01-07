430563f072fa2cd76b36b4dacdb99b8d50272331
Wynter Woods <zerotri@gmail.com>
2015-01-06 16:08:16 -0800

    Initial Commit

---
+++ Arkesia/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arkesia
+//
+//  Created by Wynter Woods on 1/6/15.
+//  Copyright (c) 2015 Wynter Woods. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
